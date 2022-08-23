edf0ab6756154bb887dbfcde6a316334f7da9356
Drayang <dchu0011@student.monash.edu>
2022-08-23 18:00:06 +0800

    Update speed

--- milestone2/operate.py
+++ milestone2/operate.py
@@ -195,16 +195,16 @@ class Operate:
             ########### replace with your M1 codes ###########
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                self.command['motion']= [2.5,0]
+                self.command['motion']= [2,0]
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                self.command['motion']= [-2.5,0]
+                self.command['motion']= [-2,0]
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                self.command['motion']= [0,2]
+                self.command['motion']= [0,2.5]
             # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                self.command['motion']= [0,-2]
+                self.command['motion']= [0,-2.5]
             ####################################################
             # stop
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
